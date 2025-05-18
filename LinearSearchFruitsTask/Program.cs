a5816d52b0f3d537946d6e7e148837af00609905
Abdurrahman Abbas <22AAbbas@heckgrammar.co.uk>
2025-05-18 13:38:18 +0100

    ATCG a trading card game

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -4,15 +4,39 @@
     {
         static void Main(string[] args)
         {
-            string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
 
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
+                string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
+
+                Console.WriteLine("Enter the fruit you want to find?");
+                string userInput = Console.ReadLine();
+
+                int i = 0;
+                bool found = false;
+
+                while (i < fruits.Length)
+                {
+                    if (fruits[i] == userInput)
+                    {
+                        found = true;
+                    }
+                    i++;
+                }
+
+                if (found == true)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }
         }
     }
-}
+    // Your program should:
+    // Ask the user what word they would like to find
+    // Output the message True if the word is found
+    // Output the message False if the word is not found
+
+    // You must write your own linear search routine and not use any built-in fuctions available in C#
+    // You should use meaningful variable names
